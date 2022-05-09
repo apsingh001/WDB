819aafc9be704da3b090c110df6aa1d566058b09
apsingh001 <64854893+apsingh001@users.noreply.github.com>
2022-05-09 10:20:59 +0530

    add usestates

---
+++ src/Heading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function H1(){
+    return(
+        <div>
+        <div id="heading1">Welcome to the history of IPL</div>
+        
+        </div>
+    )
+}
